142489e2e2439ed904dd3bfb251f0e4e8aec86ed
Your Name <nandasafiqalfiansyah@gmail.com>
2023-08-10 10:35:55 +0700

    updater footer

--- index.php
+++ index.php
@@ -5,6 +5,11 @@ include './template/header.php';
 include './template/from.php' ;
 
 include './template/ask.php' ;
+
+
+//footer//
+
+include './template/footer.php';
 //javascript include//
 include './template/router.php';
 ?>
